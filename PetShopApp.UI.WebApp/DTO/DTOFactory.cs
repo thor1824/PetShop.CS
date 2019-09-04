96a16567e9ce187b67c1b9ff7dcd4706dbe47b45
thor1824 <42994659+thor1824@users.noreply.github.com>
2019-09-04 13:17:43 +0200

    bob

---
+++ PetShopApp.UI.WebApp/DTO/DTOFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetShopApp.UI.WebApp.DTO
+{
+    public class DTOFactory
+    {
+        public enum DTOType { DTO_1, DTO_2
+        }
+
+        public static void CreateDTO()
+        {
+
+        }
+    }
+}
