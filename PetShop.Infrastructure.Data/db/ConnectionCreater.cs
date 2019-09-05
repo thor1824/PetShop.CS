3fe90812d319ba2afa7a36df3dcb55ae4b8912c2
thor1824 <42994659+thor1824@users.noreply.github.com>
2019-09-05 23:31:23 +0200

    Create ConnectionCreater.cs

---
+++ PetShop.Infrastructure.Data/db/ConnectionCreater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+
+namespace PetShop.Infrastructure.Data.Sqlite
+{
+    public class ConnectionCreater
+    {
+        public static SQLiteConnection CreateConnection()
+        {
+            return new SQLiteConnection("Data Source = C:/Users/thor1/source/repos/PetShop/PetShop.Infrastructure.Data/db/ParaBellumDB.db; Version = 3;");
+        }
+
+    }
+}
